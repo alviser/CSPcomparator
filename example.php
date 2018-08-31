8323b13e9980509139962585e6c67c346abb5a59
alviser <alvise@rabitti.org>
2018-08-31 17:16:44 +0200

    improved example.php clarity

--- example.php
+++ example.php
@@ -1,12 +1,23 @@
 <?php
+/*
+	this is an example toy starting point to test the CSP comparator
+	without using it throught the Wordpress plugin
+*/
+
 require_once "CSPclasses.php";
 require_once "CSPfunctions.php";
+
+$protocol = "http";
+$hostname = "example.com";
+
+$policy_source_1 = "default-src none;";
+$policy_source_2 = "default-src *;";
 
 $policy_1 = new CSPpolicy($protocol,$hostname);
 $policy_2 = new CSPpolicy($protocol,$hostname);
 
-$policy_1->policyParse($policy_1);
-$policy_2->policyParse($policy_2);
+$policy_1->policyParse($policy_source_1);
+$policy_2->policyParse($policy_source_2);
 
 if (isPolicyMoreStrict($policy_1,$policy_2)) {
 	if (isPolicyMoreStrict($policy_2,$policy_1)) {
